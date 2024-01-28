d04a8b39fecf499c535804f40627ac5111ac720c
waschmaschvanlu <waschmaschvanlu@gmail.com>
2024-01-28 19:59:20 +0100

    Added Clean Boilerplate

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+
+name "Boilerplate"
+description "Boilerplate"
+author "Ludaro"
+version "0"
+
+client_scripts {
+    "client/*.lua",
+}
+
+server_scripts {
+    "server/*.lua",
+}
+
+shared_scripts {
+    "shared/*.lua",
+}
